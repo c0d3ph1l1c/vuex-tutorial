23a4b3524978b974ef42709a75b62fea3456b13e
c0d3ph1l1c <kwl.c0d3r@gmail.com>
2020-04-19 12:56:53 +0800

    Created menu page for all sections
     	new file:   index.html
     	modified:   sections.js
     	modified:   webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,67 +4,64 @@ const HTMLWebpackPlugin = require('html-webpack-plugin');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
 module.exports = {
-  mode: 'development',
-  entry: {},
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'js/[name]-[contentHash].js'
-  },
-  devServer: {
-    index: `${sections[sections.length-1]}.html`,
-    filename: `js/${sections[sections.length-1]}.js`
-  },
-  plugins: [
-    new VueLoaderPlugin()
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              '@babel/preset-env' 
-            ],
-            plugins: [
-              '@babel/plugin-transform-runtime'
-            ]
-          }
+    mode: 'development',
+    entry: {},
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/[name]-[contentHash].js'
+    },
+    devServer: {
+        index: 'index.html'
+    },
+    plugins: [
+        new VueLoaderPlugin()
+    ],
+    module: {
+        rules: [{
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            '@babel/preset-env'
+                        ],
+                        plugins: [
+                            '@babel/plugin-transform-runtime'
+                        ]
+                    }
+                }
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.html$/i,
+                use: 'html-loader'
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: 'file-loader'
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            }
+        ]
+    },
+    resolve: {
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js'
         }
-      },
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.html$/i,
-        use: 'html-loader'
-      },
-      {
-        test: /\.(png|jpe?g|gif)$/i,
-        use: 'file-loader'
-      },
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader'
-      }
-    ]
-  },
-  resolve: {
-    alias: {
-      'vue$': 'vue/dist/vue.esm.js'
     }
-  }
 };
 
 sections.forEach(section => {
-  module.exports.entry[section] = `./src/js/${section}.js`;
-  module.exports.plugins.push(new HTMLWebpackPlugin({
-    title: section,
-    filename: `${section}.html`,
-    template: `./src/${section}.html`,
-    chunks: [section]
-  }));
+    module.exports.entry[section] = `./src/js/${section}.js`;
+    module.exports.plugins.push(new HTMLWebpackPlugin({
+        filename: `${section}.html`,
+        template: `./src/${section}.html`,
+        chunks: [section]
+    }));
 });
